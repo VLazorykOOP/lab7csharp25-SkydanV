77c3599a6fcec23d7ecf91da983d4f1401dfad9d
SkydanV <you@example.com>
2025-05-14 22:49:59 +0300

    \\\

--- Lab7CSharp/Form1.Designer.cs
+++ Lab7CSharp/Form1.Designer.cs
@@ -1,17 +1,9 @@
-﻿
-namespace Lab7CSharp
+﻿namespace Lab7CSharp
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,48 +13,111 @@ namespace Lab7CSharp
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
-
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbLine = new System.Windows.Forms.RadioButton();
+            this.rbRectangle = new System.Windows.Forms.RadioButton();
+            this.rbEllipse = new System.Windows.Forms.RadioButton();
+            this.rbCircle = new System.Windows.Forms.RadioButton();
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
+
             // label1
-            // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.PaleGreen;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label1.Location = new System.Drawing.Point(376, -1);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 18);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Lab 7.   C# ";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
+            this.label1.Text = "Lab 7. C#";
+
+            // pictureBox1
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(231, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 300);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+
+            // groupBox1
+            this.groupBox1.Controls.Add(this.rbCircle);
+            this.groupBox1.Controls.Add(this.rbEllipse);
+            this.groupBox1.Controls.Add(this.rbRectangle);
+            this.groupBox1.Controls.Add(this.rbLine);
+            this.groupBox1.Location = new System.Drawing.Point(231, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 48);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "тип примітива";
+
+            // rbLine
+            this.rbLine.AutoSize = true;
+            this.rbLine.Location = new System.Drawing.Point(6, 21);
+            this.rbLine.Name = "rbLine";
+            this.rbLine.Size = new System.Drawing.Size(82, 20);
+            this.rbLine.TabIndex = 4;
+            this.rbLine.TabStop = true;
+            this.rbLine.Text = "Відрізок";
+            this.rbLine.UseVisualStyleBackColor = true;
+
+            // rbRectangle
+            this.rbRectangle.AutoSize = true;
+            this.rbRectangle.Location = new System.Drawing.Point(94, 21);
+            this.rbRectangle.Name = "rbRectangle";
+            this.rbRectangle.Size = new System.Drawing.Size(115, 20);
+            this.rbRectangle.TabIndex = 5;
+            this.rbRectangle.TabStop = true;
+            this.rbRectangle.Text = "\tПрямокутник";
+            this.rbRectangle.UseVisualStyleBackColor = true;
+
+            // rbEllipse
+            this.rbEllipse.AutoSize = true;
+            this.rbEllipse.Location = new System.Drawing.Point(215, 22);
+            this.rbEllipse.Name = "rbEllipse";
+            this.rbEllipse.Size = new System.Drawing.Size(65, 20);
+            this.rbEllipse.TabIndex = 6;
+            this.rbEllipse.TabStop = true;
+            this.rbEllipse.Text = "Еліпс";
+            this.rbEllipse.UseVisualStyleBackColor = true;
+
+            // rbCircle
+            this.rbCircle.AutoSize = true;
+            this.rbCircle.Location = new System.Drawing.Point(297, 22);
+            this.rbCircle.Name = "rbCircle";
+            this.rbCircle.Size = new System.Drawing.Size(60, 20);
+            this.rbCircle.TabIndex = 7;
+            this.rbCircle.TabStop = true;
+            this.rbCircle.Text = "Коло";
+            this.rbCircle.UseVisualStyleBackColor = true;
+
             // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 386);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
-        #endregion
-
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbLine;
+        private System.Windows.Forms.RadioButton rbRectangle;
+        private System.Windows.Forms.RadioButton rbEllipse;
+        private System.Windows.Forms.RadioButton rbCircle;
     }
 }
-
--- Lab7CSharp/Form1.cs
+++ Lab7CSharp/Form1.cs
@@ -1,25 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lab7CSharp
 {
     public partial class Form1 : Form
     {
+        Point startPoint;
+        bool isDrawing = false;
+
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
+            isDrawing = true;
+            startPoint = e.Location;
+        }
 
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!isDrawing) return;
+            isDrawing = false;
+            Point endPoint = e.Location;
+
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Pen pen = new Pen(Color.Black, 2);
+
+            // Визначаємо, який примітив вибрано
+            if (rbLine.Checked)
+            {
+                g.DrawLine(pen, startPoint, endPoint);
+            }
+            else if (rbRectangle.Checked)
+            {
+                g.DrawRectangle(pen, GetRectangle(startPoint, endPoint));
+            }
+            else if (rbEllipse.Checked)
+            {
+                g.DrawEllipse(pen, GetRectangle(startPoint, endPoint));
+            }
+            else if (rbCircle.Checked)
+            {
+                Rectangle rect = GetRectangle(startPoint, endPoint);
+                int size = Math.Min(rect.Width, rect.Height);
+                g.DrawEllipse(pen, new Rectangle(rect.X, rect.Y, size, size));
+            }
+
+            pictureBox1.Invalidate(); // Оновлюємо картинку
+        }
+
+        private Rectangle GetRectangle(Point p1, Point p2)
+        {
+            return new Rectangle(
+                Math.Min(p1.X, p2.X),
+                Math.Min(p1.Y, p2.Y),
+                Math.Abs(p2.X - p1.X),
+                Math.Abs(p2.Y - p1.Y)
+            );
+        }
+
+        // Додаємо цей метод, якщо його немає:
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Тут може бути код для завантаження форми, якщо потрібно
         }
     }
 }
